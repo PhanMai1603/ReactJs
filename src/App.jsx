5a72f2f784f4ef2963a778241d47e6cb5e71623e
PhanMai1603 <inet.ngocmai.v6@gmail.com>
2024-03-27 04:14:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
+
+import useMediaQuery from '@mui/material/useMediaQuery'
+import {useColorScheme} from '@mui/material/styles'
+
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { Mode } from '@mui/icons-material'
+
+import  LightModeIcon  from '@mui/icons-material/LightMode'
+import  DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import  SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    // setAge(event.target.value);
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{display: 'flex', alignItems: 'center', gap: '8px'}}>
+            <LightModeIcon fontSize='small'/> Light
+          </div>
+        </MenuItem>
+
+        <MenuItem value="dark">
+          <Box sx={{display: 'flex', alignItems: 'center', gap: 1}}>
+            <DarkModeOutlinedIcon  fontSize='small'/> Dark
+          </Box>
+        </MenuItem>
+
+        <MenuItem value="system">
+          <Box sx={{display:'flex', alignItems:'center', gap: 2}}>
+            <SettingsBrightnessIcon fontSize='small' />  System
+          </Box>
+        </MenuItem>
+
+      </Select>
+    </FormControl>
+  );
+}
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+function App(){
+  return (
+    <> 
+    <ModeSelect/>
+    <hr/>
+    <ModeToggle />
+    <hr/>
+    <div>PhanMai</div>
+    <Typography variant="body2" color="text.secondary">Test Typography</Typography>
+
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <br />
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[500] }} />
+    </>
+  )
+}
+
+export default App
